d2361c83f0c87edf80774929ed5d8df56d6c3db0
pepe <jose@adminisatorsmbp.lan>
2018-10-08 23:02:20 -0700

    prompts for data

--- course1/json/makejson.go
+++ course1/json/makejson.go
@@ -1,7 +1,27 @@
-// Package main has minimum Go declarations to compile a program
-// that does nothing
+// Package main prompts user for Name and Address
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	var name, address string
+	name = PromptFor("Name")
+	fmt.Println("name is", name)
+	address = PromptFor("Address")
+	fmt.Println("address is", address)
+}
+
+// PromptFor prompts for an attribute and returns the value entered from stdin
+func PromptFor(attrib string) (value string) {
+	prompt := "Enter " + attrib + ": "
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(prompt)
+	if ok := scanner.Scan(); ok {
+		value = scanner.Text()
+	}
+	return
 }

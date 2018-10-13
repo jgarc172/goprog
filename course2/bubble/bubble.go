27d6c672a69eb33b5b9b45615a3613361d89298c
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-13 09:50:19 -0700

    implemented isSorted and inOrder

--- course2/bubble/bubble.go
+++ course2/bubble/bubble.go
@@ -22,7 +22,22 @@ func Sort(ints []int) {
 
 }
 
+// isSorted returns weather or not ints is sorted
 func isSorted(ints []int) (sorted bool) {
-	sorted = false
+	// ints is a slice with int values at locations
+	// from 0 to N
+	// ints is sorted if all pairs of adjacent
+	// values are in order
+	N := len(ints) - 1
+	for i := 0; i < N-1; i++ {
+		if !inOrder(ints[i], ints[i+1]) {
+			return
+		}
+	}
+	sorted = true
 	return
+}
+
+func inOrder(a, b int) bool {
+	return a <= b
 }

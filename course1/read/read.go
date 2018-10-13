e299ea972ecc640e1c150d6c1750e8f8e5ce34a1
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-12 18:47:48 -0700

    readName func

--- course1/read/read.go
+++ course1/read/read.go
@@ -3,18 +3,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 )
 
 func main() {
 	names := make([]name, 0, 3)
-	var n name
-	var f, l string
-	fmt.Scanln(&f, &l)
-	n = name{f, l}
-	names = append(names, n)
-	fmt.Scanln(&f, &l)
-	n = name{f, l}
-	names = append(names, n)
+	n, err := readName()
+	if err == nil {
+		names = append(names, n)
+	}
 	fmt.Println(names)
 	printNames(names)
 }
@@ -24,6 +21,16 @@ type name struct {
 	lname string
 }
 
+func readName() (name, error) {
+	var n name
+	var f, l string
+	_, err := fmt.Scanln(&f, &l)
+	if err != nil {
+		return n, io.EOF
+	}
+	n = name{f, l}
+	return n, nil
+}
 func printNames(names []name) {
 	for _, n := range names {
 		fmt.Println(n.fname, n.lname)

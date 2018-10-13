40f1602e7b5cec40aa426ee07d0ae0fb631d9fe5
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-13 09:39:28 -0700

    defined Sort and isSorted

--- course2/bubble/bubble.go
+++ course2/bubble/bubble.go
@@ -1,6 +1,28 @@
-// Package bubble is a minimum executable program
+// Package bubble provides a Sort function that sorts a slice of integers
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	ints := make([]int, 4)
+	Sort(ints)
+	sorted := isSorted(ints)
+	fmt.Println("Sorted?:", sorted)
+	ints = []int{3, 1, 4, 2}
+	Sort(ints)
+	sorted = isSorted(ints)
+	fmt.Println("Sorted?:", sorted)
+
+}
+
+// Sort sorts slice ints
+func Sort(ints []int) {
+
+}
+
+func isSorted(ints []int) (sorted bool) {
+	sorted = false
+	return
 }

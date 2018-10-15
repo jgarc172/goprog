3183648a274f499f543cd0a58e03a8ae09bbc9f7
pepe <jose@adminisatorsmbp.lan>
2018-10-14 23:37:16 -0700

    define GenGenDisplaceFn Displacement

--- course2/motion/motion.go
+++ course2/motion/motion.go
@@ -1,6 +1,20 @@
-// Package motion defines a minimum executable go program
+// Package motion defines functions of motion
 package main
 
 func main() {
 
+}
+
+type time float64
+
+// Displacement as a function of time
+type Displacement func(time) float64
+
+// GenDisplaceFn returns a
+func GenDisplaceFn(a, v0, s0 float64) Displacement {
+
+	fn := func(time) float64 {
+		return 0
+	}
+	return fn
 }

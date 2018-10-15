aaa8ce6be4fd033934d538983749692e42c7b4b9
pepe <jose@adminisatorsmbp.lan>
2018-10-14 23:50:23 -0700

    prints displacements for values of time

--- course2/motion/motion.go
+++ course2/motion/motion.go
@@ -1,20 +1,23 @@
 // Package motion defines functions of motion
+// and prints displacements for two values of time
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fn := GenDisplaceFn(10, 2, 1)
+	fmt.Println(fn(3))
+	fmt.Println(fn(5))
 }
 
-type time float64
-
 // Displacement as a function of time
-type Displacement func(time) float64
+type Displacement func(float64) float64
 
-// GenDisplaceFn returns a
-func GenDisplaceFn(a, v0, s0 float64) Displacement {
-
-	fn := func(time) float64 {
-		return 0
+// GenDisplaceFn returns a Displacement function
+func GenDisplaceFn(a, v0, s0 float64) (fn Displacement) {
+	// s = ½ a t2 + vot + so
+	fn = func(t float64) float64 {
+		return 0.5*a*t*t + v0*t + s0
 	}
-	return fn
+	return
 }

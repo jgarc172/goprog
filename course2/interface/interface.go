09c1b952780c471d7edc8129a00499fbc84dea2c
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-18 17:21:55 -0700

    basic shell

--- course2/interface/interface.go
+++ course2/interface/interface.go
@@ -1,6 +1,20 @@
-// Package interface is a minimal program
+// Package interface provides a shell
+// to prompt the user for an animal name
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var name string
+	usage := "Enter: animal-name | exit"
+	fmt.Println(usage)
+	for name != "exit" {
+		fmt.Print("> ")
+		fmt.Scanln(&name)
+		if name != "exit" {
+			fmt.Println(name)
+		}
+	}
 }

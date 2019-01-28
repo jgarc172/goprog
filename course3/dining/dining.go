c5a6072855b13899103be7edc061610fb5e5ff9f
jgarc172 <jgarc172@mail.ccsf.edu>
2019-01-27 16:07:33 -0800

    refactored dining

--- course3/dining/dining.go
+++ course3/dining/dining.go
@@ -9,54 +9,6 @@ import (
 	"sync"
 )
 
-type ChopS struct{ sync.Mutex }
-
-type Philo struct {
-	id              int
-	leftCS, rightCS *ChopS
-}
-
-func (p Philo) eat() {
-	for i := 0; i < 3; i++ {
-		p.leftCS.Lock()
-		p.rightCS.Lock()
-
-		fmt.Printf("starting to eat %d\n", p.id)
-		runtime.Gosched()
-		fmt.Printf("finished eating %d\n", p.id)
-
-		p.rightCS.Unlock()
-		p.leftCS.Unlock()
-	}
-	done <- true
-}
-
-type Host struct {
-}
-
-func (h Host) canEat(p *Philo) {
-	canEat <- p
-}
-func (h Host) serve() {
-	fullDiners := 0
-	for {
-		select {
-		case p := <-canEat:
-			go p.eat()
-		case <-done:
-			fullDiners++
-			if fullDiners == 5 {
-				wg.Done()
-				return
-			}
-		}
-	}
-}
-
-var wg sync.WaitGroup
-var done = make(chan bool)
-var canEat = make(chan *Philo, 2)
-
 func main() {
 	CSticks := make([]*ChopS, 5)
 	for i := 0; i < 5; i++ {
@@ -64,14 +16,79 @@ func main() {
 	}
 	philos := make([]*Philo, 5)
 
-	host := new(Host)
-	wg.Add(1)
-	go host.serve()
+	host := NewHost(philos, CSticks)
+	host.Serve()
+}
+
+// ChopS is a representation of a single chopstick
+type ChopS struct{ sync.Mutex }
+
+// Philo is a representation of a Dining Philosopher
+type Philo struct {
+	id              int
+	leftCS, rightCS *ChopS
+	host            *Host
+}
+
+func (p Philo) eat() {
+	for i := 0; i < 3; i++ {
+		p.leftCS.Lock()
+		p.rightCS.Lock()
+		fmt.Printf("starting to eat %d\n", p.id)
+
+		runtime.Gosched()
+
+		fmt.Printf("finished eating %d\n", p.id)
+		p.rightCS.Unlock()
+		p.leftCS.Unlock()
+	}
+	p.host.Done()
+}
+
+// Host coordinates dinner for the Dining Philosophers
+type Host struct {
+	philos  []*Philo
+	cSticks []*ChopS
+	canEat  chan *Philo
+	done    chan bool
+}
+
+// NewHost creates a new instance of Host
+func NewHost(phils []*Philo, chs []*ChopS) *Host {
+	canEat := make(chan *Philo, 2)
+	var done = make(chan bool)
+
+	return &Host{phils, chs, canEat, done}
+}
+func (h Host) waitOn(p *Philo) {
+	h.canEat <- p
+}
+
+//Done is called by diners after finishing eating
+func (h Host) Done() {
+	h.done <- true
+}
+
+//Serve starts serving diners until finished
+func (h Host) Serve() {
 
 	for i := 0; i < 5; i++ {
-		philos[i] = &Philo{i, CSticks[i], CSticks[(i+1)%5]}
-		go host.canEat(philos[i])
+		h.philos[i] = &Philo{i, h.cSticks[i], h.cSticks[(i+1)%5], &h}
+		go h.waitOn(h.philos[i])
 	}
 
-	wg.Wait()
+	dinersServed := 0
+	for {
+		select {
+		case p := <-h.canEat:
+			go p.eat()
+		case <-h.done:
+			dinersServed++
+			fmt.Println("\nhost finished serving a diner")
+			fmt.Println()
+			if dinersServed == 5 {
+				return
+			}
+		}
+	}
 }
